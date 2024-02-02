52c0d10bbeca9c86d8c3e51d05e94ff1dd24873b
Muhammad Abdullah <abdulx125@gmail.com>
2024-02-03 01:37:36 +0500

    sign in start from tommorow

--- src/Components/Navbars/Navbar.jsx
+++ src/Components/Navbars/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import { Button, Flex } from "antd";
 import { Typewriter } from "react-simple-typewriter";
 
 /* <h1>Abdul Fatah</h1> */
@@ -15,26 +16,33 @@ import { Typewriter } from "react-simple-typewriter";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar w-full min-h-12 bg-blue-800">
-        <div className="flex  flex-wrap">
-          <Typewriter
-            words={[
-              "Hey There",
-              "Sign In to Get Amazing Deals",
-              "Best Products",
-              "Enjoyed Deals",
-            ]}
-            loop={0}
-            cursor
-            cursorStyle="|"
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />{" "}
-        </div>
-        <div className="flex flex-wrap">
-          <button>Sign In</button>
-          <button>Log In</button>
+      <nav className="navbar w-full min-h-12 p-2 flex bg-blue-800">
+        <div className="flex justify-between w-full mx-10">
+          <div>
+            <Typewriter
+              words={[
+                "Hey There",
+                "Sign In to Get Amazing Deals",
+                "Best Products",
+                "Enjoyed Deals",
+              ]}
+              loop={0}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />{" "}
+          </div>
+          <div className="flex items-center ">
+            <Button type="primary" ghost>
+              Sign In
+            </Button>
+
+            <Button type="primary" danger ghost>
+              Log In
+            </Button>
+          </div>
         </div>
       </nav>
     </>
