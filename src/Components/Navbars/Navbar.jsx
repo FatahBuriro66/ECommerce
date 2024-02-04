af97775c3399ecad0de7c92d336ef72c25a032a3
Muhammad Abdullah <abdulx125@gmail.com>
2024-02-04 15:04:45 +0500

    react-router-error          Uncaught Error: You cannot render a <Router> inside another <Router>. You should never have more than one in your app.

--- src/Components/Navbars/Navbar.jsx
+++ src/Components/Navbars/Navbar.jsx
@@ -1,17 +1,18 @@
+// /* <h1>Abdul Fatah</h1> */
+// /* <div className="links">
+//               <Link to="/">Home</Link>
+//               <Link to="/services">Services</Link>
+//               <Link to="/work">Work</Link>
+//               <Link to="/contactUs">Contact Us</Link>
+//               <Link to="/SignIn">Sign In</Link>
+//               <Link to="/SignUp">Sign Up</Link> */
+
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import { Button, Flex } from "antd";
 import { Typewriter } from "react-simple-typewriter";
-
-/* <h1>Abdul Fatah</h1> */
-/* <div className="links">
-              <Link to="/">Home</Link>
-              <Link to="/services">Services</Link>
-              <Link to="/work">Work</Link>
-              <Link to="/contactUs">Contact Us</Link>
-              <Link to="/SignIn">Sign In</Link>
-              <Link to="/SignUp">Sign Up</Link> */
+import Register from "../../Pages/Resgistration/Register/Register";
 
 const Navbar = () => {
   return (
@@ -35,16 +36,18 @@ const Navbar = () => {
             />{" "}
           </div>
           <div className="flex items-center ">
-            <Button type="primary" ghost>
-              Sign In
-            </Button>
-
+            <Link to="/register">
+              <Button type="primary" ghost>
+                Sign In
+              </Button>
+            </Link>
             <Button type="primary" danger ghost>
               Log In
             </Button>
           </div>
         </div>
       </nav>
+      <div className="bg-green-700 w-full h-4">checking</div>
     </>
   );
 };
